eb3ba5ad51709a57c24ce06d903a751cd1219d12
roken47 <69013261+roken47@users.noreply.github.com>
2020-09-18 11:50:18 -0500

    button needs debugging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,19 @@
-let t1Shots = document.getElementById("teamone-numshots").innerHTML;
-let t1Goals = document.getElementById("teamone-numgoals").innerHTML;
-let t1Button = document.getElementById("teamone-shoot-button").innerHTML;
-let t2Shots = document.getElementById("teamtwo-numshots").innerHTML;
-let t2Goals = document.getElementById("teamtwo-numgoals").innerHTML;
-let t2Button = document.getElementById("teamtwo-shoot-button").innerHTML;
-let resetButton = document.getElementById("reset-button").innerHTML;
-let resetCount = document.getElementById("num-resets").innerHTML;
+let teamOneShots = document.getElementById("teamone-numshots");
+let teamOneGoals = document.getElementById("teamone-numgoals");
+let teamOneButton = document.getElementById("teamone-shoot-button");
+let teamOneShotsCount = 0;
+let teamOneGoalsCount = 0;
+let teamTwoShots = document.getElementById("teamtwo-numshots");
+let teamTwoGoals = document.getElementById("teamtwo-numgoals");
+let teamTwoButton = document.getElementById("teamtwo-shoot-button");
+let teamTwoShotsCount = 0;
+let teamTwoGoalsCount = 0;
+let resetButton = document.getElementById("reset-button");
+let resetTxtCount = document.getElementById("num-resets");
+let resetNumCount = 0;
+// Event Listeners after Local Scope Variables have been Initialized
+teamOneButton.addEventListener("Click", function () {
+  teamOneShotsCount++;
+  teamOneShots.innerHTML = teamOneShotsCount;
+  console.log(teamOneShotsCount);
+});
